9ff34bed510ae33548807712b9e868f0a7009220
Sofah <sofahheng@gmail.com>
2026-03-02 14:58:11 +0700

    Add static export config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Add this line
-  // images: { unoptimized: true } // Usually required for static exports
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
