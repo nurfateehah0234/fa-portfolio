1988a404d9213fa73776d9dc37e1d013736726b9
Sofah <sofahheng@gmail.com>
2026-03-02 15:00:21 +0700

    Add static export config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // <--- THIS IS THE MAGIC LINE
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for static export
   },
+  // If you have swcMinify: true, delete it to remove that warning!
 };
 
 module.exports = nextConfig;
